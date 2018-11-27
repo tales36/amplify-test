46e9eb98e314c98f44032157112821eaa3ba8f7e
tales36 <hakuoro36@yahoo.co.jp>
2018-11-27 15:32:34 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ 
+<?php
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false) {
+?>
+<h3>strposが非falseを返しました</h3>
+<center><b>あなたはInternet Explorerを使用しています</b></center>
+<?php
+} else {
+?>
+<h3>strposがfalseを返しました</h3>
+<center><b>あなたはInternet Explorerを使用していません</b></center>
+<?php
+}
+?> 
+
+</body>
+</html> 
