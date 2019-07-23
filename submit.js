1bd8e1f0bbd209cbbcb05141f18dc64e5145f51d
tales36 <hakuoro36@yahoo.co.jp>
2019-07-23 14:29:38 +0900

    Add files via upload

---
+++ submit.js
@@ -0,0 +1,24 @@
+// ボタンの要素を取得
+    var button = document.getElementById("button");     
+// ボタンをクリックした時の処理
+    button.addEventListener("click", function(e) {
+    e.preventDefault();
+// 入力フォームの値を取得
+    var name = document.getElementById("name").value;
+    var mail = document.getElementById("mail").value;
+    var sex = document.getElementById("sex").value;
+    var blood = document.getElementById("blood").value;
+    var textform = document.getElementById("textform").value;
+    var form = name + mail + sex + blood + textform;
+    function sendmail(subject, body) {
+      return fetch('https://iy93xy8xg3.execute-api.us-east-1.amazonaws.com/production/submit', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ subject, body })
+      })
+    }
+    sendmail("お問い合わせがありました",form);
+    });
